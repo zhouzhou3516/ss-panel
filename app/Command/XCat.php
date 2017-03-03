b4ec46f9fc2b77dc4bd1336db6bf5a731cae1c16
zhouzhou3516 <735338750@qq.com>
2017-03-03 22:48:27 +0800

    email to multi user

--- app/Command/XCat.php
+++ app/Command/XCat.php
@@ -112,7 +112,12 @@ class XCat
         $ret = Zzf::zzfggcx();
         $title = '自住房-' . (($ret == null)?'没有新公告':'有新公告');
         $body  = ($ret == null? '没有新公告':$ret);
-        Mail::sendSMTP('735338750@qq.com',$title,$body);
+        $to_array = array("1025541660@qq.com",'735338750@qq.com');
+        foreach ($to_array as $to)
+        {
+            Mail::sendSMTP($to,$title,$body);
+        }
+        //Mail::sendSMTP('735338750@qq.com;1025541660@qq.com',$title,$body);
         print('sent email');
     }
 }
