bb4c4ed8ff522283ed649b872d93b7a8c474f154
zhouzhou3516 <735338750@qq.com>
2017-03-09 08:43:36 +0800

    cron execute time zone :minus 8 hours

--- app/Command/XCat.php
+++ app/Command/XCat.php
@@ -112,7 +112,7 @@ class XCat
         $ret = Zzf::zzfggcx();
         $title = '自住房-' . (($ret == null)?'没有新公告':'有新公告');
         $body  = ($ret == null? '没有新公告':$ret);
-        $to_array = array("1025541660@qq.com",'735338750@qq.com');
+        $to_array = array("1025541660@qq.com",'735338750@qq.com','1009056230@qq.com','357890780@qq.com');
         foreach ($to_array as $to)
         {
             Mail::sendSMTP($to,$title,$body);
