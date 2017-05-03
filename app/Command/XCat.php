7e0e92a0ec233bca014b8e301696aa84c2b28ca4
zhouzhou3516 <735338750@qq.com>
2017-05-03 08:30:09 +0800

    1. add debug Config 2. add BASE_PATH

--- app/Command/XCat.php
+++ app/Command/XCat.php
@@ -112,8 +112,13 @@ class XCat
         $ret = Zzf::zzfggcx();
         $title = '自住房-' . (($ret == null)?'没有新公告':'有新公告') . '-QingZhouLee';
         $body  = ($ret == null? '没有新公告':$ret);
-        $to_array = array("1025541660@qq.com",'735338750@qq.com','1009056230@qq.com','357890780@qq.com','781598458@qq.com');
-        //$to_array = array('735338750@qq.com');
+        $to_array = null;
+        if (Config::get('debug') == "true") {
+            $to_array = array('735338750@qq.com');
+        }else{
+            $to_array = array("1025541660@qq.com",'735338750@qq.com','1009056230@qq.com','357890780@qq.com','781598458@qq.com');
+        }
+        //
         if($ret != null )
         {
             foreach ($to_array as $to)
--- app/Services/Zzf.php
+++ app/Services/Zzf.php
@@ -2,6 +2,7 @@
 namespace App\Services;
 
 use DOMDocument;
+define('BASE_PATH', __DIR__ . '/../../');
 /**
  * Created by PhpStorm.
  * User: qingzhli
@@ -13,7 +14,7 @@ class Zzf
 {
     public static function zzfggcx()
     {
-        $myfile = fopen("./storage/logs/zzfnews.log",'a+') or die("Unable to open file:zzfnews.log!");
+        $myfile = fopen(BASE_PATH."/storage/logs/zzfnews.log",'a+') or die("Unable to open file:zzfnews.log!");
 
         $base_url='http://www.bjjs.gov.cn';
         $tzgx_list_url='http://www.bjjs.gov.cn/bjjs/fwgl/zzxspzf/tzgg/index.shtml';
@@ -32,16 +33,22 @@ class Zzf
                     }
                     $ggdate = $i->nodeValue;
                     $dataPattern = "/^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$/";
-
-                    if(preg_match($dataPattern, $ggdate) and $ggdate == date("Y-m-d"))
+                    $today = date("Y-m-d");
+                    if(Config::get("debug")=='true'){
+                        $today = '2017-04-27';
+                    }
+                    if(preg_match($dataPattern, $ggdate) and $ggdate == $today)
                         //if($i -> nodeValue == date("Y-m-d"))
                     {
                         $c_url = $base_url.$firstNode->getAttributeNode('href')->value;
                         $title = $firstNode->nodeValue;
                         $line  = '<a href=\''.$c_url.'\'>'.$title.'</a>  '.$ggdate.'<br>';
 
-                        if(self::isexits($title.'-'.$ggdate."\n")==0){
+                        if(self::is_exist($title.'-'.$ggdate."\n")==0){
                             fwrite($myfile,$title.'-'.$ggdate."\n");
+                            $res_string = $res_string.$line;
+                        }
+                        if(Config::get("debug")=='true'){
                             $res_string = $res_string.$line;
                         }
 
@@ -59,10 +66,10 @@ class Zzf
      * @param $title
      * @return int
      */
-    public static function isexits($title){
+    public static function is_exist($title){
         $is_exist = 0;
-        if(file_exists("./storage/logs/zzfnews.log") and !empty($title)){
-            $myfile = fopen("./storage/logs/zzfnews.log",'r') or die("Unable to open file:zzfnews.log!");
+        if(file_exists(BASE_PATH."/storage/logs/zzfnews.log") and !empty($title)){
+            $myfile = fopen(BASE_PATH."./storage/logs/zzfnews.log",'r') or die("Unable to open file:zzfnews.log!");
             // 输出单行直到 end-of-file
             while(!feof($myfile)) {
                 $line = fgets($myfile);
